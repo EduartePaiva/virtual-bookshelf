9d97da2853e33cc3507eb6f56d6cef7c2a02f3f2
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-05-07 21:02:03 -0300

    building the logic to redirect to /login if user is not logged in

--- planning.md
+++ planning.md
@@ -1,6 +1,8 @@
 ## what I want for this project?
 
-1. have login setup, (gh oauth)
-2. Be able to search books from the open library api, https://openlibrary.org/search.json?q=harry+potter
-3. Be able to add this book + metadata + image to the dataset
-4. books state, (reading, completed, unread)
+[x] - have login setup, (gh oauth)
+[_] - access app only after login.
+[_] - have a flag to limit number of users to 1
+[_] - Be able to search books from the open library api, https://openlibrary.org/search.json?q=harry+potter
+[_] - Be able to add this book + metadata + image to the dataset
+[_] - books state, (reading, completed, unread)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	import { enhance } from '$app/forms';
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href={resolve('/login')}>login</a>
+
+<h1>Hi, {data.user.name}!</h1>
+<p>Your user ID is {data.user.id}.</p>
+<form method="post" action="?/signOut" use:enhance>
+	<button class="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
+		>Sign out</button
+	>
+</form>
